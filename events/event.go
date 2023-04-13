c2de4efe50fecfa2a5afc69ab856b33bc4f12517
lucasvillalbaar <lucas.a.villalba@gmail.com>
2023-04-13 08:19:49 -0300

    defining messages and events

---
+++ events/event.go
@@ -0,0 +1,32 @@
+package events
+
+import (
+	"context"
+
+	"github.com/lucasvillalbaar/go-events-cqrs/models"
+)
+
+type EventStore interface {
+	Close()
+	PublishCreatedFeed(ctx context.Context, feed *models.Feed) error
+	SubscribeCreatedFeed(ctx context.Context) (<-chan CreatedFeedMessage, error)
+	OnCreatedFeed(ctx context.Context, f func(CreatedFeedMessage)) error
+}
+
+var eventStore EventStore
+
+func Close() {
+	eventStore.Close()
+}
+
+func PublishCreatedFeed(ctx context.Context, feed *models.Feed) error {
+	return eventStore.PublishCreatedFeed(ctx, feed)
+}
+
+func SubscribeCreatedFeed(ctx context.Context) (<-chan CreatedFeedMessage, error) {
+	return eventStore.SubscribeCreatedFeed(ctx)
+}
+
+func OnCreatedFeed(ctx context.Context, f func(CreatedFeedMessage)) error {
+	return eventStore.OnCreatedFeed(ctx, f)
+}
